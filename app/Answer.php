0795d720eb2861501b4b76a2edd6cd4ce9857ae2
medobelk <cynerdemid@gmail.com>
2017-12-20 16:38:30 +0200

    YURA DONT TOUCH SCRIPTS JQUERY IS ALREADY IN IT

--- app/Answer.php
+++ app/Answer.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class Answer extends Model
 {
     public function question()
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,6 +1,7 @@
 <!-- <script src="js/jquery.min.js"></script> -->
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script src="{{ asset('js/jquery.min.js') }}"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="{{ asset('js/jquery.min.js') }}"></script> -->
+<script src="{{ asset('js/compiled.js') }}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
 <!-- <script src="{{ asset('js/owl.carousel.min.js') }}"></script> -->
 
