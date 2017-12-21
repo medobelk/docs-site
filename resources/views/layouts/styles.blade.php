b4a6e30e2f88b7cbff34aa615106ead1d6fadc58
medobelk <cynerdemid@gmail.com>
2017-12-21 15:14:14 +0200

    Merge branch 'master' of https://github.com/medobelk/docs-site

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -2,7 +2,6 @@
 <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}"/>
 <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}"/>
 <link rel="stylesheet" href="{{ asset('css/main.css') }}"/>
-<link rel="stylesheet" href="{{ asset('css/zoom-marker.css') }}">
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
